d33a99ab81c8a3bd707f53daf8aa71323c09a127
Leo <leonidamvrosov@yandex.ru>
2023-06-13 20:43:56 +0500

    methods sumAll,printArray added

--- src/main/java/ru/gb/hw4/Main.java
+++ src/main/java/ru/gb/hw4/Main.java
@@ -2,6 +2,29 @@ package ru.gb.hw4;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[][] arr = {{"1", "1", "1", "1"}, {"2", "2", "2", "2"}, {"3", "3", "3", "3"}, {"4", "4", "4", "4"}};
+        printArray(arr);
+        System.out.println();
+        System.out.println(sumAll(arr));
+
+    }
+
+    public static int sumAll(String[][] inputArray) {
+        int sum = 0;
+        for (String[] strings : inputArray) {
+            for (String string : strings) {
+                sum += Integer.parseInt(string);
+            }
+        }
+        return sum;
+    }
+
+    public static void printArray(String[][] array) {
+        for (String[] s : array) {
+            for (String str : s) {
+                System.out.printf(str + " ");
+            }
+            System.out.print(" ");
+        }
     }
 }
